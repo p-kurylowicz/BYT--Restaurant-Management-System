a64ed3d14b27e19fc490b652db30f659c09f8fad
YuriiSaviak <s27506@pjwstk.edu.pl>
2025-12-17 18:53:31 +0100

    Update DineIn.java

--- src/DineIn.java
+++ src/DineIn.java
@@ -4,32 +4,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class DineIn extends Order {
+public class DineIn implements OrderRole {
     @Serial
     private static final long serialVersionUID = 1L;
 
-
     private LocalDateTime servingStartTime;
 
-    
-    private List<Table> tables;
+    private final List<Table> tables;
     private Reservation reservation;
 
-    public DineIn(Customer customer) {
-        super(customer);
+    public DineIn() {
         this.tables = new ArrayList<>();
         this.servingStartTime = LocalDateTime.now();
         this.reservation = null;
     }
 
-    public DineIn(Customer customer, Reservation reservation) {
-        super(customer);
-        this.tables = new ArrayList<>();
-        this.servingStartTime = LocalDateTime.now();
+    public DineIn(Reservation reservation) {
+        this();
         setReservation(reservation);
     }
 
-    
+    @Override
+    public OrderKind kind() {
+        return OrderKind.DINE_IN;
+    }
+
     public LocalDateTime getServingStartTime() { return servingStartTime; }
     public Reservation getReservation() { return reservation; }
 
@@ -37,7 +36,6 @@ public class DineIn extends Order {
         return Collections.unmodifiableList(tables);
     }
 
-    
     public void setServingStartTime(LocalDateTime servingStartTime) {
         if (servingStartTime == null) {
             throw new IllegalArgumentException("Serving start time cannot be null");
@@ -46,10 +44,8 @@ public class DineIn extends Order {
     }
 
     public void setReservation(Reservation reservation) {
-        
         this.reservation = reservation;
     }
-
 
     public void addTable(Table table) {
         if (table == null) {
@@ -61,22 +57,16 @@ public class DineIn extends Order {
         }
     }
 
-
     public void releaseTables() {
         for (Table table : tables) {
             table.changeStatus(TableStatus.AVAILABLE);
         }
-    }
-
-    @Override
-    public void completeOrder() {
-        super.completeOrder();
-        releaseTables();
+        tables.clear();
     }
 
     @Override
     public String toString() {
-        return String.format("DineIn[servingStartTime=%s, tables=%d, hasReservation=%s, %s]",
-            servingStartTime, tables.size(), reservation != null, super.toString());
+        return String.format("DineIn[servingStartTime=%s, tables=%d, hasReservation=%s]",
+                servingStartTime, tables.size(), reservation != null);
     }
 }
